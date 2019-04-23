9719891f7fb862135dd91171aad5daa615c70933
leyvalc <leyvalc@HGDLAPLEYVALC>
2019-04-23 17:15:52 -0500

    Empezar la ventana de New Assignment

---
+++ MainFrame_UI/Project_A/Controllers/NewAssignmentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project_A.Controllers
+{
+    public class NewAssignmentController : Controller
+    {
+        // GET: NewAssignment
+        public ActionResult New()
+        {
+            return View();
+        }
+    }
+}
---
+++ MainFrame_UI/Project_A/Models/NewAssignmentModels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_A.Models
+{
+    public class NewAssignmentModels
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Proyecto { get; set; }
+
+        public Sede Sede { get; set; }
+
+        public Responsable Responsable { get; set; }
+
+        
+    }
+}
