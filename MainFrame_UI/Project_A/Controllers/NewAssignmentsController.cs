9a4a3185db7bd1ac33d9efe62c7a2185adb9fcdd
leyvalc <leyvalc@HGDLAPLEYVALC>
2019-04-24 17:55:38 -0500

    Modificacion de New Assignment

---
+++ MainFrame_UI/Project_A/Controllers/NewAssignmentsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Project_A.Models;
+
+namespace Project_A.Controllers
+{
+    public class NewAssignmentsController : Controller
+    {
+
+        // GET: NewAssignments
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult New(string empleado)
+        {
+            NewAssignment newAssignment = new NewAssignment() {NPersonal = empleado };
+            //acciones para pobtener datos
+           
+
+
+            return View(newAssignment);
+
+        }
+    }
+}
+
+
+        
---
+++ MainFrame_UI/Project_A/Models/NewAssignment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Project_A.Models
+{
+    public class NewAssignment
+    {
+        public int ID { get; set; }
+        public string Folio { get; set; }
+        public DateTime Fecha { get; set; }
+        public string NPersonal { get; set; }
+        public string Username { get; set; }
+        public string Nombre { get; set; }
+        public string Sede { get; set; }
+        public string Proyecto { get; set; }
+        public string Responsable { get; set; }
+
+    }
+
+    
+}
