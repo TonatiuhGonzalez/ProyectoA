4f8aa4da57b3f607947e0b00b4feabf347457793
Teresita Gretey Rangel Dadda <rangeltg@globalhitss.com>
2019-04-24 17:55:13 -0500

    nueva interfaz (la chida)

---
+++ MainFrame_UI/Project_A/Controllers/UsuarioController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project_A.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult login()
+        {
+            return View();
+        }
+
+        //POST: Usuario
+        public ActionResult validacion(string usuario, string psw)
+        {
+            if  usuario == CONEXIONSQLUSUARIO && psw == CONEXIONSQLPSW ;
+            {
+                return RedirectToAction("Users", "Home", null);
+            }
+
+            else
+            {
+                return MENSAJEDEERROR
+            }
+
+            
+        }
+
+    }
+}
