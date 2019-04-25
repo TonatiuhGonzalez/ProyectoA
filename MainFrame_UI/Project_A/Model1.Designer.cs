f02cea40b5435ad037a184f937965e9f5ec9dab7
gonzalezst <gonzalezst@HGDLGONZALEZST>
2019-04-25 12:22:44 -0500

    Puta khe sad

--- MainFrame_UI/Project_A/Model1.Designer.cs
+++ MainFrame_UI/Project_A/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\vazquezpauc\Source\Repos\TonatiuhGonzalez\ProyectoA\MainFrame_UI\Project_A\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\gonzalezst\Source\Repos\TonatiuhGonzalez\ProyectoA\MainFrame_UI\Project_A\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
